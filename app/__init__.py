1a6ffa7c8c7900e4029f881727117a85764d2f57
ShivamSharma01496 <s.sharma01496@gmail.com>
2020-06-18 15:48:31 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -36,13 +36,13 @@ def create_app(config_class=Config):
     babel.init_app(app)
     app.elasticsearch = Elasticsearch([app.config['ELASTICSEARCH_URL']]) \
         if app.config['ELASTICSEARCH_URL'] else None
-
+    '''
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
 
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
-
+    '''
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
 
